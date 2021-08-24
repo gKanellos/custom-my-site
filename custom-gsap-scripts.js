ca65a756afd073371092f8b58739a9dbe3a9e5d9
Gabrielle Kan <gkanellos3@gmail.com>
2021-08-24 16:06:36 -0700

    Update custom-gsap-scripts.js

--- custom-gsap-scripts.js
+++ custom-gsap-scripts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
        window.requestAnimationFrame(function() {
       
         gsap.from(".split-text-group .split-text__word1", { opacity: 0, y:20, delay:3, stagger:0.15});
-        gsap.to(material2, { opacity: 1, delay:4});
+        gsap.to(material2, { opacity: 1, delay:4.5, duration:2});
 
        });
       
