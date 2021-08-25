8a272f1326d34f1e7fd369e1a40d6cf8828452bb
Gabrielle Kan <gkanellos3@gmail.com>
2021-08-25 13:48:24 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
 <?php get_header(); ?>
 
-<section class="container">
+<section class="container-about">
 <div class="section section-content clearfix  " data-arts-os-animation="animated">
                     <div class="section-content__inner">
                         <div class="section-content__wrapper-heading">
@@ -80,6 +80,37 @@
   
 </section>
 
+<section class="elementor-section elementor-top-section elementor-element elementor-element-752a8e3 elementor-section-height-full elementor-section-boxed elementor-section-height-default elementor-section-items-middle">
+    <div class="elementor-container elementor-column-gap-default">
+
+            <a class="contact-item">
+              <div class="contact-item-title">
+              <h4>Github</h4>
+              <p class="animated animatedFadeInUp fadeInUp">See some of my projects</p>
+              </div>
+            </a>
+            <a class="contact-item">
+             <div class="contact-item-title">
+              <h4>CodePen</h4>
+              <p class="animated animatedFadeInUp fadeInUp">Explore my code pens</p>
+              </div>
+            </a>
+            <a class="contact-item">
+              <div class="contact-item-title">
+              <h4>LinkedIn</h4>
+              <p class="animated animatedFadeInUp fadeInUp">Connect with me</p>
+              </div>
+            </a>
+            <a class="contact-item">
+              <div class="contact-item-title">
+              <h4>Email</h4>
+              <p class="animated animatedFadeInUp fadeInUp">Reach out</p>
+              </div>
+            </a>
+       
+    </div>
+</section>
+
 
 <style>
 
@@ -92,7 +123,7 @@ h4 {
   color:white !important;
 }
 
-.container {
+.container-about {
   /*height: 100vh;*/
   margin-top:50vh;
   display: grid;
@@ -142,6 +173,134 @@ img{
 
 .image2 {
   left:20%;
+}
+/*  Social */
+
+
+@media only screen and (min-width: 769px) {
+  .social-container {
+    display: flex;
+  }
+  .contact-item {
+  flex-grow: 1;
+    flex-basis: 0;
+    display:flex;
+    transition: 0.4s all;
+     border: 1px solid #f31c29;
+  height: 250px;
+  }
+  .contact-item:hover {
+  flex-grow: 1.3;
+  background:#f31c29;
+}
+
+.contact-item:nth-child(2) {
+    margin:0px 25px;
+}
+.contact-item:nth-child(3) {
+    margin:0px 25px 0px 0px;
+}
+  .contact-item-title p{
+  display:none;
+}
+  
+.contact-item:hover .contact-item-title{
+  color:#0f1b28;
+ 
+}
+
+.contact-item:hover .contact-item-title p {
+  display:block;
+  
+}
+
+
+@keyframes fadeInUp {
+    from {
+        transform: translate3d(0,40px,0)
+    }
+
+    to {
+        transform: translate3d(0,0,0);
+        opacity: 1
+    }
+}
+
+@-webkit-keyframes fadeInUp {
+    from {
+        transform: translate3d(0,40px,0)
+    }
+
+    to {
+        transform: translate3d(0,0,0);
+        opacity: 1
+    }
+}
+
+.animated {
+    animation-duration: 1s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 1s;
+    -webkit-animation-fill-mode: both
+}
+
+.animatedFadeInUp {
+    opacity: 0
+}
+
+.fadeInUp {
+    opacity: 0;
+    animation-name: fadeInUp;
+    -webkit-animation-name: fadeInUp;
+}
+.contact-item-title p{
+  font-size:20px !important;
+}
+
+}
+
+@media only screen and (max-width: 768px) {
+  .contact-item-title {
+     border: 1px solid #f31c29;
+    padding:1em;
+  }
+}
+
+/* end media query */
+
+.social-container {
+  justify-content: center;
+  align-items: center;
+  /*
+  height: 100vh;
+  padding-right: 25px;
+  padding-left: 25px;
+  margin-right: auto;
+  margin-left: auto;
+  */
+}
+
+.contact-item {
+ 
+  padding: 2em;
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.contact-item-title h4 {
+  text-align:left;
+  font-weight:500;
+}
+.contact-item-title{
+  text-transform: uppercase;
+  letter-spacing: 0.075em;
+  color:white;
+}
+.contact-item-title p{
+  text-transform: uppercase;
+  letter-spacing: 0.075em;
+  text-align:left;
+  font-size:1em;
 }
 
 
