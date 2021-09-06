554ab2564cb4dd1ef60ec0bc8f4c81ab45a81686
Gabrielle Kan <gkanellos3@gmail.com>
2021-09-06 12:59:26 -0700

    Update homepg.php

--- homepg.php
+++ homepg.php
@@ -112,7 +112,7 @@
 
         .scroll-down{
         position:absolute;
-        top:80%;
+        top:70%;
         display:block;
         text-align:center;
         right:50%;
@@ -167,19 +167,19 @@
             opacity: 0} );
         // image1 delta
         const image1 = new THREE.Mesh(geometry, material)
-        image1.position.set(1.6, -.7)
+        image1.position.set(1.05, -.7)
         //image 2 chronaly top
         const image2 = new THREE.Mesh(geometry, material2)
-        image2.position.set(0, .7)
+        image2.position.set(-0.35, .7)
         //image 3 newsroom top
         const image3 = new THREE.Mesh(geometry, material3)
-        image3.position.set(1.6, .7)
+        image3.position.set(1.05, .7)
         //image 4 night haze
         const image4 = new THREE.Mesh(geometry, material4)
-        image4.position.set(-1.6, -.7)
+        image4.position.set(-1.75, -.7)
         // image 5 one water top
         const image5 = new THREE.Mesh(geometry, material5)
-        image5.position.set(-1.6, .7)
+        image5.position.set(-1.75, .7)
 
         scene.add(image1, image2, image3, image4, image5)
         
